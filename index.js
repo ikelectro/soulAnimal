f5e874b9ae0ba4d56a9aca08d3edba6f34ce645b
ikelectro <39769308+ikelectro@users.noreply.github.com>
2026-09-18 20:16:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 
-
 function randomNumberGen(){
-
-var userName = prompt("Enter Your Name here");
-while(userName.length <=3){                          //checking if string value. assured name input from User
-var userName = prompt("Please Enter Your Name");
+if(attempt>=1){
+  alert("Your soul animal is "+animalName[randomNumbers]);//for avoiding multiple try
 }
+else{
+var userName = prompt("Enter Your Name here");
 
+while(userName.length <=3){                          //checking the string value for assured name input from User
+  var userName = prompt("Please Enter Your Name, more than 3 charecters are required");
+  }
+
+
+ randomNumbers = Math.floor(Math.random() * 25)+1;//adding 1 to resolve the 0 issue.
+document.querySelector("img").setAttribute("src", "./images/"+randomNumbers+".png");// setting the image url
+document.getElementsByClassName("animal-text")[0].innerHTML = "Hey! "+userName+ ", " +animalName[randomNumbers] + " is your soul animal!";
+attempt++;
+
+}
+}
 var animalName = [" ","Deer", "Crocodile","Gold Fish","Baby Zebra","baby Elephant",
                   "Horse", "Owl", "Rihno", "Baby Tiger","Hippo",
                   "Pigeon", "Unicorn", "Giraffe", "Puppy",
                   "Cow", "Kitten","Baby Goat", "Baby Chimp",
                   "Baby Lion", "Monkey","Peacock", "Red Panda", 
-                  "Panda", "Bear", "Black Panther"]  
+                  "Panda", "Bear", "Black Panther"];
 
-var randomNumbers = Math.floor(Math.random() * 25)+1;//adding 1 to resolve the 0 issue.
-document.querySelector("img").setAttribute("src", "./images/"+randomNumbers+".png");// setting the image url
-document.getElementsByClassName("animal-text")[0].innerHTML = "Hey! "+userName+ ", " +animalName[randomNumbers] + " is your soul animal!";
-
-}
-//document.getElementById("userName").addEventListener("enter", randomNumberGen);
+ var attempt = 0;
+ var randomNumbers;
 document.getElementsByClassName("btn")[0].addEventListener("click", randomNumberGen); 
+
