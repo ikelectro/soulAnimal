0fa5f6ca7fa591a11dab41d558f0ef0e62f00dd5
ikelectro <39769308+ikelectro@users.noreply.github.com>
2026-09-18 12:03:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function randomNumberGen(){
+
+var userName = prompt("Enter Your Name here");
+var animalName = [" ","Deer", "Crocodile","Gold Fish","Baby Zebra","baby Elephant",
+                  "Horse", "Owl", "Rihno", "Baby Tiger","Hippo",
+                  "Pigeon", "Unicorn", "Giraffe", "Puppy",
+                  "Cow", "Kitten","Baby Goat", "Baby Chimp",
+                  "Baby Lion", "Monkey","Peacock", "Red Panda", 
+                  "Panda", "Bear", "Black Panther"]  
+
+var randomNumbers = Math.floor(Math.random() * 25)+1;//adding 1 to resolve the 0 issue.
+document.querySelector("img").setAttribute("src", "./images/"+randomNumbers+".png");// setting the image url
+document.getElementsByClassName("animal-text")[0].innerHTML = "Hey! "+userName+ ", " +animalName[randomNumbers] + " is your soul animal!";
+}
+//document.getElementById("userName").addEventListener("enter", randomNumberGen);
+document.getElementsByClassName("btn")[0].addEventListener("click", randomNumberGen); 
